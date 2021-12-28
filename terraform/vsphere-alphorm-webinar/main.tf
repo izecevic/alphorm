24f80e1fab2b41a85dc6ccd193006de152814d87
Igor Zecevic <igor.zecevic.89@gmail.com>
2021-12-28 11:20:07 +0100

    alphorm

---
+++ terraform/vsphere-alphorm-webinar/main.tf
@@ -0,0 +1,9 @@
+# Provider
+provider "vsphere" {
+  user           = "administrator@vsphere.local"
+  password       = "MY_PASSWORD"
+  vsphere_server = "192.168.1.91"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
